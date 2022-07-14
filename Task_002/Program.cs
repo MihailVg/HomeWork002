8b5a250cffad887006d39425516c9be2038dd8b9
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-07-14 11:52:42 +0300

    Task002 done

---
+++ Task_002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача N2, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число, третью цифру которого хотите узнать: ");
+
+int numberA = int.Parse(Console.ReadLine()!);
+int numberB = 0;
+
+if(numberA < 100)
+{
+    Console.WriteLine("Данное число не имеет третьей цифры");
+}
+
+else{
+    while(numberA > 1000)
+    {
+        numberA = numberA / 10;
+    }
+
+    numberB = numberA % 10;
+    Console.WriteLine("Третьей цифрой заданного числа является " + numberB);
+}
