f885280e6f483f7158a91918090b3f3da671ceab
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-07-14 12:15:22 +0300

    Task004 done

---
+++ Task_004/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача N4, которая принимает на вход цифру, обозначающую день недели, а на выходе наименования дня
+
+Console.WriteLine("Введите цифру, обозначающую день недели, чтобы проверить его наименование: ");
+
+int numberA = int.Parse(Console.ReadLine()!);
+
+if(numberA == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+if(numberA == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+if(numberA == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+if(numberA == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+if(numberA == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+if(numberA == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+if(numberA == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+if(numberA < 1)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+
+if(numberA > 7)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
